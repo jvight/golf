aea6ea18a4e9e6c11e35a466fd542be008b75264
bao <baodq@taap.vn>
2022-02-21 11:20:52 +0700

    fix trajectory

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -91,8 +91,6 @@ public class GameController : MonoBehaviour
                 GameLose();
             }
         }
-        Debug.Log(whitePlank.Count);
-        Debug.Log(whiteDone);
     }
 
     public void UpdateFlagFly()
--- Assets/Scripts/DrawTrajectory.cs
+++ Assets/Scripts/DrawTrajectory.cs
@@ -7,13 +7,23 @@ public class DrawTrajectory : MonoBehaviour
     [SerializeField]
     private LineRenderer _lineRenderer;
     [SerializeField]
+    private GameObject ballPrefab;
+    [SerializeField]
+    public Transform dotParent;
     [Range(3, 30)]
     private int _lineSegmentCount = 20;
     private List<Vector3> _linePoint = new List<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
-
+        for (int i = 0; i < _lineSegmentCount; i++)
+        {
+            float scl = i;
+            GameObject dot = Instantiate(ballPrefab);
+            dot.transform.parent = dotParent;
+            dot.transform.localScale -= new Vector3(scl / 50, scl / 50, scl / 50);
+            Debug.Log(scl / 50);
+        }
     }
 
     public void UpdateTrajectory(Vector3 forceVector, Rigidbody rb, Vector3 startingPoint)
@@ -33,12 +43,42 @@ public class DrawTrajectory : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(startingPoint, -MovementVector, out hit, MovementVector.magnitude))
             {
+                Debug.Log(hit.collider.tag);
+                for (int j = 0; j < dotParent.childCount; j++)
+                {
+                    if (j >= i)
+                    {
+                        dotParent.GetChild(j).gameObject.SetActive(false);
+                    }
+                }
                 break;
             }
-            _linePoint.Add(-MovementVector + startingPoint);
+            // _linePoint.Add(-MovementVector + startingPoint);
+            // if (dotParent.childCount < _lineSegmentCount)
+            // {
+            //     OnCreateDot(-MovementVector + startingPoint, i);
+            // }
+            // else
+            // {
+            // }
+            dotParent.GetChild(i).gameObject.SetActive(true);
+            dotParent.GetChild(i).transform.position = -MovementVector + startingPoint;
+            // Debug.Log(dotParent.GetChild(i));
+            // if (dotParent.GetChild(i) == null) {
+            //     OnCreateDot(-MovementVector + startingPoint);
+            // }
         }
-        _lineRenderer.positionCount = _linePoint.Count;
-        _lineRenderer.SetPositions(_linePoint.ToArray());
+
+        // _lineRenderer.positionCount = _linePoint.Count;
+        // _lineRenderer.SetPositions(_linePoint.ToArray());
+    }
+
+    public void OnCreateDot(Vector3 pos, float scl)
+    {
+        GameObject dot = Instantiate(ballPrefab);
+        dot.transform.parent = dotParent;
+        dot.transform.position = pos;
+        dot.transform.localScale -= new Vector3(scl / 100, scl / 100, scl / 100);
     }
 
     public void HideLine()
